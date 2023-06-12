e1458db335e4bf030ef6b3c04d4d415f54f99cec
objektwerks <objektwerks@runbox.com>
2023-06-12 11:39:55 -0400

    edited ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -10,49 +10,10 @@ CREATE TABLE account (
   deactivated BIGINT NOT NULL
 );
 
-CREATE TABLE pool (
+CREATE TABLE profile (
   id BIGSERIAL PRIMARY KEY,
   license VARCHAR(36) REFERENCES account(license),
-  name VARCHAR(24) NOT NULL,
-  volume INT NOT NULL,
-  unit VARCHAR NOT NULL
-);
-
-CREATE TABLE cleaning (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  brush BOOL NOT NULL,
-  net BOOL NOT NULL,
-  skimmer_basket BOOL NOT NULL,
-  pump_basket BOOL NOT NULL,
-  pump_filter BOOL NOT NULL,
-  vacuum BOOL NOT NULL,
-  cleaned BIGINT NOT NULL
-);
-
-CREATE TABLE measurement (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  total_chlorine INT NOT NULL,
-  free_chlorine INT NOT NULL,
-  combined_chlorine NUMERIC(2, 1) NOT NULL,
-  ph NUMERIC(2, 1) NOT NULL,
-  calcium_hardness INT NOT NULL,
-  total_alkalinity INT NOT NULL,
-  cyanuric_acid INT NOT NULL,
-  total_bromine INT NOT NULL,
-  salt INT NOT NULL,
-  temperature INT NOT NUll,
-  measured BIGINT NOT NULL
-);
-
-CREATE TABLE chemical (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  typeof VARCHAR NOT NULL,
-  amount NUMERIC(5, 2),
-  unit VARCHAR NOT NULL,
-  added BIGINT NOT NULL
+  name VARCHAR NOT NULL
 );
 
 CREATE TABLE fault (
