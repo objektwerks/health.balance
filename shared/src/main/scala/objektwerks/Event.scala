51f467bd73893ecc611c521b18fe94a5d521a053
objektwerks <objektwerks@runbox.com>
2023-06-13 10:19:03 -0400

    added fault to event

--- shared/src/main/scala/objektwerks/Event.scala
+++ shared/src/main/scala/objektwerks/Event.scala
@@ -1,5 +1,9 @@
 package objektwerks
 
+import java.time.Instant
+
+import scalafx.beans.property.ObjectProperty
+
 sealed trait Event
 
 final case class Authorized(isAuthorized: Boolean) extends Event
@@ -9,3 +13,10 @@ final case class LoggedIn(account: Account) extends Event
 
 final case class Deactivated(account: Account) extends Event
 final case class Reactivated(account: Account) extends Event
+
+object Fault:
+  def apply(message: String, throwable: Throwable): Fault = Fault(s"$message ${throwable.getMessage}")
+
+final case class Fault(cause: String, occurred: Long = Instant.now.getEpochSecond) extends Event:
+  val causeProperty = ObjectProperty[String](this, "cause", cause)
+  val occurredProperty = ObjectProperty[Long](this, "occurred", occurred)
