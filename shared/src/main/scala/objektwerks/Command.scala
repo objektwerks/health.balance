1448316ecf82dc0b42fd0740129a73a91f19e6ec
objektwerks <objektwerks@runbox.com>
2023-06-13 10:09:46 -0400

    added register and login commands

--- shared/src/main/scala/objektwerks/Command.scala
+++ shared/src/main/scala/objektwerks/Command.scala
@@ -4,3 +4,6 @@ sealed trait Command
 
 sealed trait License:
   val license: String
+
+final case class Register(emailAddress: String) extends Command
+final case class Login(emailAddress: String, pin: String) extends Command
