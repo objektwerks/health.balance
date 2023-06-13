a046fe512ccd17ded604fa9d2f9659e6771500af
objektwerks <objektwerks@runbox.com>
2023-06-13 10:10:20 -0400

    added deactivate and reactivate commands

--- shared/src/main/scala/objektwerks/Command.scala
+++ shared/src/main/scala/objektwerks/Command.scala
@@ -7,3 +7,6 @@ sealed trait License:
 
 final case class Register(emailAddress: String) extends Command
 final case class Login(emailAddress: String, pin: String) extends Command
+
+final case class Deactivate(license: String) extends Command with License
+final case class Reactivate(license: String) extends Command with License
