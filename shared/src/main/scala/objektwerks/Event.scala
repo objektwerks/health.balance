ee74e5f0ef0fd3a78658ce5e8aaaaa3f427fd036
objektwerks <objektwerks@runbox.com>
2023-06-13 10:12:11 -0400

    added deactivated and reactivated events

--- shared/src/main/scala/objektwerks/Event.scala
+++ shared/src/main/scala/objektwerks/Event.scala
@@ -6,3 +6,6 @@ final case class Authorized(isAuthorized: Boolean) extends Event
 
 final case class Registered(account: Account) extends Event
 final case class LoggedIn(account: Account) extends Event
+
+final case class Deactivated(account: Account) extends Event
+final case class Reactivated(account: Account) extends Event
