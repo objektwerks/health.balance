65e4f411505a2502513aea4cda2a201c7ad02b60
objektwerks <objektwerks@runbox.com>
2023-06-19 09:50:51 -0400

    building out dispatcher

--- server/src/main/scala/objektwerks/Dispatcher.scala
+++ server/src/main/scala/objektwerks/Dispatcher.scala
@@ -1,4 +1,17 @@
 package objektwerks
 
 final class Dispatcher(store: Store, emailer: Emailer):
-  def dispatch[E <: Event](command: Command): Event = ???
+  def dispatch[E <: Event](command: Command): Event =
+    if !command.isValid then Fault(s"Command is invalid: $command")
+    isAuthorized(command) match
+      case Authorized(isAuthorized) => if !isAuthorized then Fault(s"License is unauthorized: $command")
+      case fault @ Fault(_, _) => fault
+      case _ =>
+        
+    command match
+      case Register(emailAddress)          => register(emailAddress)
+      case Login(emailAddress, pin)        => login(emailAddress, pin)
+      case Deactivate(license)             => deactivateAccount(license)
+      case Reactivate(license)             => reactivateAccount(license)
+      case ListProfiles(license)           => listProfiles(license)
+      case AddProfile(_, profile)          => addProfile(profile)
