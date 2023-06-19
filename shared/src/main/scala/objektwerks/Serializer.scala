b321c3fefad378e3034c6b3ca0cf9b26698b4b51
objektwerks <objektwerks@runbox.com>
2023-06-19 12:28:12 -0400

    enhanced serializer

--- shared/src/main/scala/objektwerks/Serializer.scala
+++ shared/src/main/scala/objektwerks/Serializer.scala
@@ -1,3 +1,8 @@
 package objektwerks
 
-object Serializer
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.github.plokhotnyuk.jsoniter_scala.macros.*
+
+object Serializer:
+  given JsonValueCodec[Entity] = JsonCodecMaker.make[Entity]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[Account] = JsonCodecMaker.make[Account]( CodecMakerConfig.withDiscriminatorFieldName(None) )
