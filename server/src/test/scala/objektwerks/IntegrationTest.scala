b6b3f5b690af11dcd10a30cfc2ce3c65d42643fa
objektwerks <objektwerks@runbox.com>
2023-06-20 12:27:56 -0400

    added test.conf; enhanced integration test

---
+++ server/src/test/scala/objektwerks/IntegrationTest.scala
@@ -0,0 +1,21 @@
+package objektwerks
+
+import com.typesafe.config.ConfigFactory
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration.*
+import scala.sys.process.Process
+
+// import Validator.*
+
+class IntegrationTest extends AnyFunSuite with Matchers:
+  val exitCode = Process("psql -d healthbalance -f ddl.sql").run().exitValue()
+  exitCode shouldBe 0
+
+  val config = ConfigFactory.load("test.conf")
+
+  val store = Store(config, Store.cache(minSize = 1, maxSize = 1, expireAfter = 1.hour))
+  val emailer = Emailer(config)
+  val dispatcher = Dispatcher(store, emailer)
