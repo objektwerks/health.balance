c16b71cc74adda3814c2a424ed4c62bdce5130cd
objektwerks <objektwerks@runbox.com>
2023-06-21 11:43:02 -0400

    added alerts

---
+++ client/src/main/scala/objektwerks/dialog/Alerts.scala
@@ -0,0 +1,33 @@
+package objektwerks.dialog
+
+import java.awt.Toolkit
+
+import scalafx.scene.control.{Alert, ButtonType}
+import scalafx.scene.control.Alert.AlertType
+import scalafx.stage.Stage
+
+import objektwerks.Context
+
+object Alerts:
+  val centerX = Toolkit.getDefaultToolkit.getScreenSize.width / 2.4
+  val centerY = Toolkit.getDefaultToolkit.getScreenSize.height / 6
+
+  def showRegisterAlert(context: Context, stage: Stage): Option[ButtonType] =
+    new Alert(AlertType.Error) {
+      initOwner(stage)
+      title = context.windowTitle
+      headerText = context.buttonRegister
+      contentText = context.errorRegister
+      x = centerX
+      y = centerY
+    }.showAndWait()
+
+  def showLoginAlert(context: Context, stage: Stage): Option[ButtonType] =
+    new Alert(AlertType.Error) {
+      initOwner(stage)
+      title = context.windowTitle
+      headerText = context.buttonLogin
+      contentText = context.errorLogin
+      x = centerX
+      y = centerY
+    }.showAndWait()
