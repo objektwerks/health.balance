a6f8e3da0d6aaf2cf8ebc27e639923fc50b6f0f2
objektwerks <objektwerks@runbox.com>
2023-06-21 11:27:26 -0400

    enhanced client

--- client/src/main/scala/objektwerks/Client.scala
+++ client/src/main/scala/objektwerks/Client.scala
@@ -7,8 +7,19 @@ import scalafx.application.JFXApp3
 
 object Client extends JFXApp3 with LazyLogging:
   private val conf = ConfigFactory.load("client.conf")
-  println(conf)
+  private val context = Context(conf)
+  //private val fetcher = Fetcher(context)
+  //private val model = Model(fetcher)
 
-  override def start(): Unit = logger.info("*** Client stopped.")
+  override def start(): Unit =
+    //val view = View(context, model)
+    stage = new JFXApp3.PrimaryStage:
+      //scene = view.scene
+      title = context.windowTitle
+      minWidth = context.windowWidth
+      minHeight = context.windowHeight
+      icons.add(context.logo)
+
+    stage.hide()
 
   override def stopApp(): Unit = logger.info("*** Client stopped.")
