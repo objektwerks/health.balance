eb51d63fd8151c1e1d5d2121c607b4c186a9ffdd
objektwerks <objektwerks@runbox.com>
2023-06-21 11:33:13 -0400

    added model

---
+++ client/src/main/scala/objektwerks/Model.scala
@@ -0,0 +1,13 @@
+package objektwerks
+
+import com.typesafe.scalalogging.LazyLogging
+
+import scalafx.application.Platform
+import scalafx.beans.property.ObjectProperty
+
+final class Model(fetcher: Fetcher) extends LazyLogging:
+  val shouldBeInFxThread = (message: String) => require(Platform.isFxApplicationThread, message)
+  val shouldNotBeInFxThread = (message: String) => require(!Platform.isFxApplicationThread, message)
+
+  val registered = ObjectProperty[Boolean](true)
+  val loggedin = ObjectProperty[Boolean](true)
