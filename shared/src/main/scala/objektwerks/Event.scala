9d220ab1c2e436ce35cdcb6773d2f008cd3742ce
objektwerks <objektwerks@runbox.com>
2023-06-23 04:42:07 -0400

    added ordering to fault

--- shared/src/main/scala/objektwerks/Event.scala
+++ shared/src/main/scala/objektwerks/Event.scala
@@ -35,6 +35,8 @@ final case class MeasurableAdded(id: Long) extends Event
 final case class MeasurableUpdated(id: Long) extends Event
 
 object Fault:
+  given faultOrdering: Ordering[Fault] = Ordering.by[Fault, Long](f => f.occurred).reverse
+
   def apply(cause: String, throwable: Throwable): Fault = Fault(cause = s"$cause ${throwable.getMessage}")
 
 final case class Fault(cause: String, occurred: Long = Instant.now.getEpochSecond) extends Event:
