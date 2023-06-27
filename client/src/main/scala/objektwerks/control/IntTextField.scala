c6767bad6a28db520765f792a2e67d529028a065
objektwerks <objektwerks@runbox.com>
2023-06-27 10:33:27 -0400

    added int text field

---
+++ client/src/main/scala/objektwerks/control/IntTextField.scala
@@ -0,0 +1,21 @@
+package objektwerks.control
+
+import scalafx.scene.control.{TextField, TextFormatter}
+import scalafx.scene.control.TextFormatter.Change
+import scalafx.util.converter.IntStringConverter
+
+object IntTextField:
+  val regex = """[0-9]+""".r
+
+class IntTextField extends TextField:
+  val converter = IntStringConverter()
+  val filter: Change => Change = { (change: Change) =>
+    if IntTextField.regex.matches(change.text) then
+      change // if integer, make change
+    else
+      change.setText("") // else make no change
+      change
+  }
+  val formatter = new TextFormatter[Int](converter, 0, filter)
+
+  textFormatter = formatter
