16d01f2ececd874c9a64ed9639c070685207e0ea
objektwerks <objektwerks@runbox.com>
2023-06-27 10:29:28 -0400

    building edible dialog

--- client/src/main/scala/objektwerks/dialog/EdibleDialog.scala
+++ client/src/main/scala/objektwerks/dialog/EdibleDialog.scala
@@ -2,18 +2,27 @@ package objektwerks.dialog
 
 import scalafx.Includes.*
 import scalafx.scene.layout.Region
-import scalafx.scene.control.{ButtonType, Dialog, TextField}
+import scalafx.scene.control.{ButtonType, ComboBox, Dialog, TextField}
 import scalafx.scene.control.ButtonBar.ButtonData
 
-import objektwerks.{Client, Context, Edible}
+import objektwerks.{Client, Context, Edible, EdibleKind}
+import scalafx.collections.ObservableBuffer
 
 final class EdibleDialog(context: Context, edible: Edible) extends Dialog[Edible]:
   initOwner(Client.stage)
   title = context.windowTitle
   headerText = context.dialogProfile
+  
+  val kindComboBox = new ComboBox[String]:
+  	items = ObservableBuffer.from( EdibleKind.toList )
+  	value = edible.kind.toString
+  kindComboBox.prefWidth = 200
 
   val detailTextField = new TextField:
     text = edible.detail
+
+  val caloriesTextField = new IntTextField:
+    text = pool.volume.toString
 
   val controls = List[(String, Region)](
     context.labelDetail -> detailTextField
