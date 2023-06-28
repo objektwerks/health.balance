850aec53b2794176b7108aba3f1eee5c66313542
objektwerks <objektwerks@runbox.com>
2023-06-28 08:20:46 -0400

    added drinkable dialog

---
+++ client/src/main/scala/objektwerks/dialog/DrinkableDialog.scala
@@ -0,0 +1,54 @@
+package objektwerks.dialog
+
+import scalafx.Includes.*
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.layout.Region
+import scalafx.scene.control.{ButtonType, CheckBox, ComboBox, Dialog, Label, TextField}
+import scalafx.scene.control.ButtonBar.ButtonData
+
+import objektwerks.{Client, Context, Drinkable, DrinkableKind, Entity}
+
+final class DrinkableDialog(context: Context, drinkable: Drinkable) extends Dialog[Drinkable]:
+  initOwner(Client.stage)
+  title = context.windowTitle
+  headerText = context.dialogEdible
+  
+  val kindComboBox = new ComboBox[String]:
+  	items = ObservableBuffer.from( DrinkableKind.toList )
+  	value = drinkable.kind
+  kindComboBox.prefWidth = 200
+
+  val detailTextField = new TextField:
+    text = drinkable.detail
+
+  val organicCheckBox = new CheckBox:
+    selected = drinkable.organic
+
+  val caloriesTextField = new IntTextField:
+    text = drinkable.calories.toString
+
+  val drankLabel = new Label:
+    text = Entity.epochSecondToLocalDateTime(drinkable.drank).toString
+
+  val controls = List[(String, Region)](
+    context.labelKind -> kindComboBox,
+    context.labelDetail -> detailTextField,
+    context.labelOrganic -> organicCheckBox,
+    context.labelCalories -> caloriesTextField,
+    context.labelDrank -> drankLabel
+  )
+  dialogPane().content = ControlGridPane(controls)
+
+  val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
+  dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
+
+  resultConverter = dialogButton => {
+    if dialogButton == saveButtonType then
+      drinkable.copy(
+        kind = kindComboBox.value.value,
+        detail = detailTextField.text.value,
+        organic = organicCheckBox.selected.value,
+        calories = caloriesTextField.text.value.toIntOption.getOrElse(drinkable.calories)
+      )
+    else null
+  }
