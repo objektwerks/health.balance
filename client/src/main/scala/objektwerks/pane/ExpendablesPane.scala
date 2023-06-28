8f5f09475c078465ab87b2fdcc0101665fcaf890
objektwerks <objektwerks@runbox.com>
2023-06-28 15:28:03 -0400

    added expendables pane

---
+++ client/src/main/scala/objektwerks/pane/ExpendablesPane.scala
@@ -0,0 +1,10 @@
+package objektwerks.pane
+
+import scalafx.geometry.Insets
+import scalafx.scene.layout.VBox
+
+import objektwerks.{Context, Model}
+
+final class ExpendablesPane(context: Context, model: Model) extends VBox:
+  spacing = 6
+  padding = Insets(6)
