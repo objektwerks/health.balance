a5e0c8a0cdb0bdb740cb2ffd8f072e467ea5b06f
objektwerks <objektwerks@runbox.com>
2023-06-30 06:41:56 -0400

    edited measurables pane

--- client/src/main/scala/objektwerks/pane/MeasurablesPane.scala
+++ client/src/main/scala/objektwerks/pane/MeasurablesPane.scala
@@ -1,9 +1,12 @@
 package objektwerks.pane
 
+import scalafx.Includes.*
 import scalafx.geometry.Insets
-import scalafx.scene.layout.VBox
+import scalafx.scene.control.{Button, SelectionMode, TableColumn, TableView}
+import scalafx.scene.layout.{HBox, Priority, VBox}
 
-import objektwerks.{Context, Model}
+import objektwerks.{Context, Measurable, Model}
+import objektwerks.dialog.MeasurableDialog
 
 final class MeasurablesPane(context: Context, model: Model) extends VBox:
   spacing = 6
