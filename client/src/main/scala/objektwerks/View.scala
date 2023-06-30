74d0246ddccb05688e3adc436d057fddb8ff1915
objektwerks <objektwerks@runbox.com>
2023-06-30 11:55:39 -0400

    enhanced view

--- client/src/main/scala/objektwerks/View.scala
+++ client/src/main/scala/objektwerks/View.scala
@@ -1,8 +1,11 @@
 package objektwerks
 
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Orientation}
 import scalafx.scene.Scene
-import scalafx.scene.layout.BorderPane
+import scalafx.scene.control.SplitPane
+import scalafx.scene.layout.{BorderPane, Priority, VBox}
+
+import objektwerks.pane.*
 
 final class View(context: Context, model: Model):
   val borderPane = new BorderPane:
@@ -10,7 +13,24 @@ final class View(context: Context, model: Model):
     prefHeight = context.windowHeight
     padding = Insets(6)
 
-  println(model)
+  // val dashboardPane = DashboardPane(context, model)
+  // HBox.setHgrow(dashboardPane, Priority.Always)
+
+  val poolsPane = ProfilesPane(context, model)
+  VBox.setVgrow(poolsPane, Priority.Always)
+
+  val tabbedPane = TabbedPane(context, model)
+  VBox.setVgrow(tabbedPane, Priority.Always)
+
+  val splitPane = new SplitPane {
+    orientation = Orientation.Horizontal
+    items.addAll(poolsPane, tabbedPane)
+  }
+  splitPane.setDividerPositions(0.30, 0.70)
+  VBox.setVgrow(splitPane, Priority.Always)
+
+  // borderPane.top = dashboardPane
+  borderPane.center = splitPane
 
   val scene = new Scene:
     root = borderPane
