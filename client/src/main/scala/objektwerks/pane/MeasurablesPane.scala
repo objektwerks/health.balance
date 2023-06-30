e5485fbed34049a0e8cf63be0adfc0a7c650a6fc
objektwerks <objektwerks@runbox.com>
2023-06-30 06:45:06 -0400

    enhanced measurables pane

--- client/src/main/scala/objektwerks/pane/MeasurablesPane.scala
+++ client/src/main/scala/objektwerks/pane/MeasurablesPane.scala
@@ -11,3 +11,23 @@ import objektwerks.dialog.MeasurableDialog
 final class MeasurablesPane(context: Context, model: Model) extends VBox:
   spacing = 6
   padding = Insets(6)
+
+  val tableView = new TableView[Measurable]():
+    columns ++= List(
+      new TableColumn[Measurable, String]:
+        text = "Kind"
+        cellValueFactory = _.value.kindProperty
+      ,
+      new TableColumn[Measurable, String]:
+        text = "Measurement"
+        cellValueFactory = _.value.measurementProperty
+      ,
+      new TableColumn[Measurable, String]:
+        text = "Unit"
+        cellValueFactory = _.value.unitProperty
+      ,
+      new TableColumn[Measurable, String]:
+        text = "Measured"
+        cellValueFactory = _.value.measuredProperty
+    )
+    items = model.observableMeasurables
