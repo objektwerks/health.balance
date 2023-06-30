bd58c74c8c3f75ea4a040738b81161b1a4f812e0
objektwerks <objektwerks@runbox.com>
2023-06-30 06:33:34 -0400

    enhanced expendable pane

--- client/src/main/scala/objektwerks/pane/ExpendablesPane.scala
+++ client/src/main/scala/objektwerks/pane/ExpendablesPane.scala
@@ -11,3 +11,37 @@ import objektwerks.dialog.ExpendableDialog
 final class ExpendablesPane(context: Context, model: Model) extends VBox:
   spacing = 6
   padding = Insets(6)
+
+  val tableView = new TableView[Expendable]():
+    columns ++= List(
+      new TableColumn[Expendable, String]:
+        text = "Kind"
+        cellValueFactory = _.value.kindProperty
+      ,
+      new TableColumn[Expendable, String]:
+        text = "Detail"
+        cellValueFactory = _.value.detailProperty
+      ,
+      new TableColumn[Expendable, Boolean]:
+        text = "Sunshine"
+        cellValueFactory = _.value.sunshineProperty
+        cellFactory = (cell, bool) => cell.text = yesOrNo(bool)
+      ,
+      new TableColumn[Expendable, Boolean]:
+        text = "Freshair"
+        cellValueFactory = _.value.freshairProperty
+        cellFactory = (cell, bool) => cell.text = yesOrNo(bool)
+      ,
+      new TableColumn[Expendable, String]:
+        text = "Calories"
+        cellValueFactory = _.value.caloriesProperty
+      ,
+      new TableColumn[Expendable, String]:
+        text = "Start"
+        cellValueFactory = _.value.startProperty
+      ,
+      new TableColumn[Expendable, String]:
+        text = "Finish"
+        cellValueFactory = _.value.finishProperty
+    )
+    items = model.observableExpendables
