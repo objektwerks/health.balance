73e61e1e30e402b7a6e193089fef44dd4f8822f8
objektwerks <objektwerks@runbox.com>
2023-06-30 06:30:37 -0400

    edited expendables pane

--- client/src/main/scala/objektwerks/pane/ExpendablesPane.scala
+++ client/src/main/scala/objektwerks/pane/ExpendablesPane.scala
@@ -1,9 +1,12 @@
 package objektwerks.pane
 
+import scalafx.Includes.*
 import scalafx.geometry.Insets
-import scalafx.scene.layout.VBox
+import scalafx.scene.control.{Button, SelectionMode, TableColumn, TableView}
+import scalafx.scene.layout.{HBox, Priority, VBox}
 
-import objektwerks.{Context, Model}
+import objektwerks.{Context, Expendable, Model}
+import objektwerks.dialog.ExpendableDialog
 
 final class ExpendablesPane(context: Context, model: Model) extends VBox:
   spacing = 6
