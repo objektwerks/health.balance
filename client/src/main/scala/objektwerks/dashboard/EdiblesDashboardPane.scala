6bb6c34d4b7bc79339c6b38727caed5e0822e95b
objektwerks <objektwerks@runbox.com>
2023-07-03 16:24:17 -0400

    enhanced edibles dashboard pane

--- client/src/main/scala/objektwerks/dashboard/EdiblesDashboardPane.scala
+++ client/src/main/scala/objektwerks/dashboard/EdiblesDashboardPane.scala
@@ -3,4 +3,4 @@ package objektwerks.dashboard
 import objektwerks.{Context, Model}
 
 final class EdiblesDashboardPane(context: Context, model: Model) extends DashboardTitledPane(context):
-  
+  text = context.tabEdibles
