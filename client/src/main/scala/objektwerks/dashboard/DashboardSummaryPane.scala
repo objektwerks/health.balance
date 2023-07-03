84b9ac0e5d1c6b6516db9cd26abf3c4d12965e9f
objektwerks <objektwerks@runbox.com>
2023-07-03 17:10:26 -0400

    renamed dashboard summary pane

--- client/src/main/scala/objektwerks/dashboard/DashboardSummaryPane.scala
+++ client/src/main/scala/objektwerks/dashboard/DashboardSummaryPane.scala
@@ -4,7 +4,7 @@ import scalafx.scene.control.TitledPane
 
 import objektwerks.Context
 
-abstract class DashboardTitledSummaryPane(context: Context) extends TitledPane:
+abstract class DashboardSummaryPane(context: Context) extends TitledPane:
   collapsible = false
   maxWidth = Double.MaxValue
   maxHeight = Double.MaxValue
