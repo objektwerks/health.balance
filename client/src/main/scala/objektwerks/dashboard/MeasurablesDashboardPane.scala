e2767afd9aa0667a7811212df81843a220579393
objektwerks <objektwerks@runbox.com>
2023-07-03 16:26:04 -0400

    enhanced measurables dashboard pane

--- client/src/main/scala/objektwerks/dashboard/MeasurablesDashboardPane.scala
+++ client/src/main/scala/objektwerks/dashboard/MeasurablesDashboardPane.scala
@@ -3,4 +3,4 @@ package objektwerks.dashboard
 import objektwerks.{Context, Model}
 
 final class MeasurablesDashboardPane(context: Context, model: Model) extends DashboardTitledPane(context):
-    
+  text = context.tabMeasurables
