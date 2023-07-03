130712acee5b891827de4a46f57a46044bf29486
objektwerks <objektwerks@runbox.com>
2023-07-03 16:21:25 -0400

    added expendables dashboard pane

---
+++ client/src/main/scala/objektwerks/dashboard/ExpendablesDashboardPane.scala
@@ -0,0 +1,6 @@
+package objektwerks.dashboard
+
+import objektwerks.{Context, Model}
+
+final class ExpendablesDashboardPane(context: Context, model: Model) extends DashboardTitledPane(context):
+  
