74f703afdba1aba01fe139fa8cbf2d962088c569
objektwerks <objektwerks@runbox.com>
2023-07-04 10:14:36 -0400

    renamed measurables dashboard

--- client/src/main/scala/objektwerks/dashboard/MeasurablesDashboard.scala
+++ client/src/main/scala/objektwerks/dashboard/MeasurablesDashboard.scala
@@ -1,6 +1,11 @@
 package objektwerks.dashboard
 
+import scalafx.scene.control.TitledPane
+
 import objektwerks.{Context, Model}
 
-final class MeasurablesDashboard(context: Context, model: Model) extends DashboardPane(context):
-   text = context.tabMeasurables
+final class MeasurablesDashboard(context: Context, model: Model) extends TitledPane:
+  collapsible = false
+  maxWidth = Double.MaxValue
+  maxHeight = Double.MaxValue
+  text = context.tabMeasurables
