b06c1033dd24d88a2d197b0d716c374def51b8be
objektwerks <objektwerks@runbox.com>
2023-07-04 10:12:15 -0400

    renamed measurables dashboard

--- client/src/main/scala/objektwerks/dashboard/MeasurablesDashboardPane.scala
+++
@@ -1,6 +0,0 @@
-package objektwerks.dashboard
-
-import objektwerks.{Context, Model}
-
-final class MeasurablesDashboardPane(context: Context, model: Model) extends DashboardPane(context):
-   text = context.tabMeasurables
