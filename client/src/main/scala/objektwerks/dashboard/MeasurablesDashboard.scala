51806f89e87a5320764f5ac157561a9ae337cc08
objektwerks <objektwerks@runbox.com>
2023-07-05 07:56:33 -0400

    enhanced measurables dashboard

--- client/src/main/scala/objektwerks/dashboard/MeasurablesDashboard.scala
+++ client/src/main/scala/objektwerks/dashboard/MeasurablesDashboard.scala
@@ -1,6 +1,8 @@
 package objektwerks.dashboard
 
+import scalafx.geometry.Insets
 import scalafx.scene.control.{Label, TitledPane}
+import scalafx.scene.layout.HBox
 
 import objektwerks.{Context, Model}
 
@@ -18,6 +20,12 @@ final class MeasurablesDashboard(context: Context, model: Model) extends TitledP
 
   val glucose = new Label:
     text = "0"
+
+  val controls = new HBox:
+    padding = Insets(6)
+    children = List(context.labelWeight, weight, context.labelPulse, pulse, context.labelGlucose, glucose)
+
+  content = controls
 
   model.observableMeasurables.onChange { (_, _) =>
   }
