56f279029f5b702761e925cc7314c1a13d5fce5e
objektwerks <objektwerks@runbox.com>
2023-07-05 08:07:54 -0400

    refactored calories in out dashboard pane

--- client/src/main/scala/objektwerks/dashboard/CaloriesInOutDashboardPane.scala
+++ client/src/main/scala/objektwerks/dashboard/CaloriesInOutDashboardPane.scala
@@ -1,29 +1,11 @@
 package objektwerks.dashboard
 
-import scalafx.scene.control.{Label, TitledPane}
-
 import objektwerks.{Context, Model}
-import objektwerks.dialog.ControlGridPane
 
-final class CaloriesInOutDashboardPane(context: Context, model: Model) extends TitledPane:
-  collapsible = false
-  maxWidth = Double.MaxValue
-  maxHeight = Double.MaxValue
+// In/Out Ration = Edibles + Drinkables / Expendables
+final class CaloriesInOutDashboardPane(context: Context, model: Model) extends CaloriesDashboardPane(context):
   text = context.tabCaloriesInOut
-
-  val in = new Label:
-    text = "0"
   
-  val out = new Label:
-    text = "0"
-
-  val controls = List[(String, Label)](
-    context.labelIn -> in,
-    context.labelOut -> out,
-  )
-  
-  content = ControlGridPane(controls)
-
   model.observableEdibles.onChange { (_, _) =>
   }
 
