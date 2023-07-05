d1eb7de3a844b32be76f5e2e6f220eecacc5b37a
objektwerks <objektwerks@runbox.com>
2023-07-05 07:28:42 -0400

    enhanced expendables dashboard pane

--- client/src/main/scala/objektwerks/dashboard/ExpendablesDashboardPane.scala
+++ client/src/main/scala/objektwerks/dashboard/ExpendablesDashboardPane.scala
@@ -2,5 +2,8 @@ package objektwerks.dashboard
 
 import objektwerks.{Context, Model}
 
-final class ExpendablesDashboardPane(context: Context, model: Model) extends DashboardPane(context):
-   text = context.tabExpendables
+final class ExpendablesDashboardPane(context: Context, model: Model) extends CaloriesDashboardPane(context):
+  text = context.tabExpendables
+
+  model.observableExpendables.onChange { (_, _) =>
+  }
