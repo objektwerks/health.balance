745ed3346c4fd261c15f16b7c34b2e92a1f55975
objektwerks <objektwerks@runbox.com>
2023-07-06 07:50:10 -0400

    enhanced calories in out dashboard pane

--- client/src/main/scala/objektwerks/dashboard/CaloriesInOutDashboardPane.scala
+++ client/src/main/scala/objektwerks/dashboard/CaloriesInOutDashboardPane.scala
@@ -5,14 +5,5 @@ import objektwerks.{Context, Model}
 // In/Out Ration = Edibles + Drinkables / Expendables
 final class CaloriesInOutDashboardPane(context: Context, model: Model) extends CaloriesDashboardPane(context):
   text = context.tabCaloriesInOut
-  today.text = "0/0"
-  week.text = "0/0"
-  
-  model.observableEdibles.onChange { (_, _) =>
-  }
-
-  model.observableDrinkables.onChange { (_, _) =>
-  }
-
-  model.observableExpendables.onChange { (_, _) =>
-  }
+  today.text <== model.caloriesInOutToday
+  week.text <== model.caloriesInOutWeek
