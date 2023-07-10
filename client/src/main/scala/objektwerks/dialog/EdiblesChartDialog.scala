9816776f25c237581b0cd6ff6a409ec543270a49
objektwerks <objektwerks@runbox.com>
2023-07-10 13:22:49 -0400

    added edibles chart dialog

---
+++ client/src/main/scala/objektwerks/dialog/EdiblesChartDialog.scala
@@ -0,0 +1,18 @@
+package objektwerks.dialog
+
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.layout.VBox
+
+import objektwerks.{Client, Context, Model}
+import objektwerks.chart.EdiblesChart
+
+final class EdiblessChartDialog(context: Context, model: Model) extends Dialog[Unit]:
+  initOwner(Client.stage)
+  title = context.windowTitle
+  headerText = context.chartCleanings
+  
+  dialogPane().buttonTypes = List(ButtonType.Close)
+  dialogPane().content = new VBox:
+    spacing = 6
+    children = List( EdiblesChart(context, model) )
