edf2fc44a4f9e016f3904f646a87c18b619b6fa1
objektwerks <objektwerks@runbox.com>
2023-07-11 11:50:12 -0400

    enhanced measurables pane

--- client/src/main/scala/objektwerks/pane/MeasurablesPane.scala
+++ client/src/main/scala/objektwerks/pane/MeasurablesPane.scala
@@ -40,9 +40,15 @@ final class MeasurablesPane(context: Context, model: Model) extends VBox:
     disable = true
     onAction = { _ => update() }
 
+  val chartButton = new Button:
+    graphic = context.chartImage
+    text = context.buttonChart
+    disable = true
+    onAction = { _ => chart() }
+
   val buttonBar = new HBox:
     spacing = 6
-    children = List(addButton, editButton)
+    children = List(addButton, editButton, chartButton)
 
   model.selectedProfileId.onChange { (_, _, _) =>
     addButton.disable = false
