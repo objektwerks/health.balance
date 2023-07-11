be41d71f3086dfa5f0c602bca997434e9b5d3f19
objektwerks <objektwerks@runbox.com>
2023-07-11 11:02:37 -0400

    added drinkables chart

--- client/src/main/scala/objektwerks/chart/DrinkablesChart.scala
+++ client/src/main/scala/objektwerks/chart/DrinkablesChart.scala
@@ -11,33 +11,33 @@ import objektwerks.{Context, Entity, Model}
 
 final case class DrinkableXY(xDate: String, yCount: Int)
 
-final class EdiblesChart(context: Context, model: Model) extends TabPane:
-  val edibles = model.observableEdibles.reverse
+final class DrinkablesChart(context: Context, model: Model) extends TabPane:
+  val drinkables = model.observableDrinkables.reverse
   val dateFormat = DateTimeFormatter.ofPattern("M.dd")
-  val minDate = Entity.epochSecondToLocalDate( drinkables.map(e => e.ate).min ).format(dateFormat)
-  val maxDate = Entity.epochSecondToLocalDate( drinkables.map(e => e.ate).max ).format(dateFormat)
+  val minDate = Entity.epochSecondToLocalDate( drinkables.map(d => d.drank).min ).format(dateFormat)
+  val maxDate = Entity.epochSecondToLocalDate( drinkables.map(d => d.drank).max ).format(dateFormat)
 
   val tab = new Tab:
     closable = false
-    text = context.tabEdibles
+    text = context.tabDrinkables
     content = buildChart()
 
   padding = Insets(6)
   tabs = List(tab)
 
   def buildChart(): LineChart[String, Number] =
-    val filtered = drinkables.map(e => DrinkableXY( Entity.epochSecondToLocalDate(e.ate).format(dateFormat), e.calories) )
+    val filtered = drinkables.map(e => DrinkableXY( Entity.epochSecondToLocalDate(e.drank).format(dateFormat), e.calories) )
     val (chart, series) = LineChartBuilder.build(context = context,
                                                  xLabel = context.chartMonthDay,
                                                  xMinDate = minDate,
                                                  xMaxDate = maxDate,
-                                                 yLabel = context.tabEdibles,
+                                                 yLabel = context.tabDrinkables,
                                                  yLowerBound = 0,
                                                  yUpperBound = 7,
                                                  yTickUnit = 1,
-                                                 yValues = filtered.map(exy => exy.yCount))
-    filtered foreach { exy =>
-      series.data() += XYChart.Data[String, Number](exy.xDate.format(dateFormat), exy.yCount)
+                                                 yValues = filtered.map(dxy => dxy.yCount))
+    filtered foreach { dxy =>
+      series.data() += XYChart.Data[String, Number](dxy.xDate.format(dateFormat), dxy.yCount)
     }
     chart.data = series
     LineChartBuilder.addTooltip(chart)
