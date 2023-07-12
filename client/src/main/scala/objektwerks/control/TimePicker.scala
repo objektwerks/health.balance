dc1f7b40bbbccf967729711d35129a6ac7c05993
objektwerks <objektwerks@runbox.com>
2023-07-12 13:22:21 -0400

    refactored time picker

--- client/src/main/scala/objektwerks/control/TimePicker.scala
+++ client/src/main/scala/objektwerks/control/TimePicker.scala
@@ -1,6 +1,5 @@
 package objektwerks.control
 
-import scalafx.collections.ObservableBuffer
 import scalafx.geometry.Insets
 import scalafx.scene.control.Spinner
 import scalafx.scene.layout.{HBox, Priority}
@@ -8,12 +7,9 @@ import scalafx.scene.layout.{HBox, Priority}
 final class TimePicker extends HBox:
   spacing = 6
   padding = Insets(6)
-
-  val amPm = ObservableBuffer[String]("AM", "PM")
   
-  val hourSpinner = Spinner[Int](min = 0, max = 12, initialValue = 1, amountToStepBy = 1)
+  val hourSpinner = Spinner[Int](min = 0, max = 23, initialValue = 1, amountToStepBy = 1)
   val minuteSpinner = Spinner[Int](min = 0, max = 59, initialValue = 1, amountToStepBy = 1)
-  val amPmSpinner = Spinner[String](items = amPm)
 
-  children = List(hourSpinner, minuteSpinner, amPmSpinner)
+  children = List(hourSpinner, minuteSpinner)
   HBox.setHgrow(this, Priority.Always)
