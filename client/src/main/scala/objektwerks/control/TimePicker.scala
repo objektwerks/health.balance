f29de3e7e343815aae549d0d3bd7ea04431eaee5
objektwerks <objektwerks@runbox.com>
2023-07-12 11:46:42 -0400

    enhanced time picker

--- client/src/main/scala/objektwerks/control/TimePicker.scala
+++ client/src/main/scala/objektwerks/control/TimePicker.scala
@@ -1,6 +1,15 @@
 package objektwerks.control
 
+import scalafx.scene.control.Spinner
 import scalafx.scene.layout.HBox
 
 final class TimePicker extends HBox:
+  val hourSpinner = new Spinner[Int]:
+    min = 0
+    max = 12
+    initialValue = 1
+    amountToStepBy = 1
+
+
+  
   
