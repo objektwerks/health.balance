e3d53ced59fad40f799842dd07a04a0360516f0f
objektwerks <objektwerks@runbox.com>
2023-07-12 13:31:01 -0400

    added constructor args to time picker

--- client/src/main/scala/objektwerks/control/TimePicker.scala
+++ client/src/main/scala/objektwerks/control/TimePicker.scala
@@ -4,15 +4,15 @@ import scalafx.geometry.Insets
 import scalafx.scene.control.{Label, Spinner}
 import scalafx.scene.layout.{HBox, Priority}
 
-final class TimePicker extends HBox:
+final class TimePicker(hour: Int, minute: Int) extends HBox:
   spacing = 3
   padding = Insets(3)
 
   val labelHour = Label("H:")
   val labelMinute = Label("M:")
   
-  val hourSpinner = Spinner[Int](min = 0, max = 23, initialValue = 1, amountToStepBy = 1)
-  val minuteSpinner = Spinner[Int](min = 0, max = 59, initialValue = 1, amountToStepBy = 1)
+  val hourSpinner = Spinner[Int](min = 0, max = 23, initialValue = hour, amountToStepBy = 1)
+  val minuteSpinner = Spinner[Int](min = 0, max = 59, initialValue = minute, amountToStepBy = 1)
 
   children = List(labelHour, hourSpinner, labelMinute, minuteSpinner)
   HBox.setHgrow(this, Priority.Always)
