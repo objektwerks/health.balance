9e3b166f83878c80172c4e229310981dde16b209
objektwerks <objektwerks@runbox.com>
2023-07-12 11:59:25 -0400

    edited time picker

--- client/src/main/scala/objektwerks/control/TimePicker.scala
+++ client/src/main/scala/objektwerks/control/TimePicker.scala
@@ -9,11 +9,11 @@ final class TimePicker extends HBox:
   spacing = 6
   padding = Insets(6)
 
-  val ampm = ObservableBuffer[String]("AM", "PM")
+  val amPm = ObservableBuffer[String]("AM", "PM")
   
   val hourSpinner = Spinner[Int](min = 0, max = 12, initialValue = 1, amountToStepBy = 1)
   val minuteSpinner = Spinner[Int](min = 0, max = 59, initialValue = 1, amountToStepBy = 1)
-  val amPmSpinner = Spinner[String](items = ampm)
+  val amPmSpinner = Spinner[String](items = amPm)
 
   children = List(hourSpinner, minuteSpinner, amPmSpinner)
   HBox.setHgrow(this, Priority.Always)
