aa98dfd9137c46fbefae41bbc55302018f9e0e52
objektwerks <objektwerks@runbox.com>
2023-08-01 13:36:42 -0400

    enhanced time picker

--- client/src/main/scala/objektwerks/control/TimePicker.scala
+++ client/src/main/scala/objektwerks/control/TimePicker.scala
@@ -2,7 +2,7 @@ package objektwerks.control
 
 import java.time.LocalTime
 
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.control.{Label, Spinner}
 import scalafx.scene.layout.{HBox, Priority}
 
@@ -10,8 +10,13 @@ final class TimePicker(localTime: LocalTime) extends HBox:
   spacing = 3
   padding = Insets(3)
 
-  val labelHour = Label("H:")
-  val labelMinute = Label("M:")
+  val labelHour = new Label:
+    alignment = Pos.CENTER_LEFT
+    text = "H:"
+
+  val labelMinute = new Label:
+    alignment = Pos.CENTER_LEFT
+    text = "M:"
   
   val hourSpinner = Spinner[Int](min = 0, max = 23, initialValue = localTime.getHour, amountToStepBy = 1)
   val minuteSpinner = Spinner[Int](min = 0, max = 59, initialValue = localTime.getMinute, amountToStepBy = 1)
