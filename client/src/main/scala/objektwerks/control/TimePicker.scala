63c023e59504df89efa880adaa9e7495e0b4966f
objektwerks <objektwerks@runbox.com>
2024-01-02 10:52:50 -0500

    refactored time picker

--- client/src/main/scala/objektwerks/control/TimePicker.scala
+++ client/src/main/scala/objektwerks/control/TimePicker.scala
@@ -2,26 +2,34 @@ package objektwerks.control
 
 import java.time.LocalTime
 
-import scalafx.geometry.{Insets, Pos}
+import scalafx.beans.property.ObjectProperty
+import scalafx.geometry.Insets
 import scalafx.scene.control.{Label, Spinner}
 import scalafx.scene.layout.{HBox, Priority}
 
-final class TimePicker(localTime: LocalTime) extends HBox:
+class TimePicker(localTime: LocalTime) extends HBox:
   spacing = 3
   padding = Insets(3)
 
+  val value = ObjectProperty[LocalTime](localTime)
+
   val labelHour = new Label:
-    alignment = Pos.CENTER_LEFT
+    prefHeight = 25
     text = "H:"
 
   val labelMinute = new Label:
-    alignment = Pos.CENTER_LEFT
+    prefHeight = 25
     text = "M:"
   
   val hourSpinner = Spinner[Int](min = 0, max = 23, initialValue = localTime.getHour, amountToStepBy = 1)
+  hourSpinner.prefWidth = 75
+  hourSpinner.value.onChange { (_, _, newValue) => value.value = value.value.withHour(newValue) }
+
   val minuteSpinner = Spinner[Int](min = 0, max = 59, initialValue = localTime.getMinute, amountToStepBy = 1)
+  minuteSpinner.prefWidth = 75
+  minuteSpinner.value.onChange { (_, _, newValue) => value.value = value.value.withMinute(newValue) }
 
   children = List(labelHour, hourSpinner, labelMinute, minuteSpinner)
   HBox.setHgrow(this, Priority.Always)
 
-  def value = localTime.withHour( hourSpinner.value.value ).withMinute( minuteSpinner.value.value )
+  def time = localTime.withHour( hourSpinner.value.value ).withMinute( minuteSpinner.value.value )
