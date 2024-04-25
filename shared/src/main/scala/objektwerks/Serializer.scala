5eb5e46c059130fcba95e0ec2e124250427e5b04
objektwerks <objektwerks@runbox.com>
2024-04-25 12:40:46 -0400

    edited serializer

--- shared/src/main/scala/objektwerks/Serializer.scala
+++ shared/src/main/scala/objektwerks/Serializer.scala
@@ -5,12 +5,12 @@ import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
 object Serializer:
   given JsonValueCodec[Entity] = JsonCodecMaker.make[Entity]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Account] = JsonCodecMaker.make[Account]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Profile] = JsonCodecMaker.make[Profile]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Edible] = JsonCodecMaker.make[Edible]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Drinkable] = JsonCodecMaker.make[Drinkable]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Expendable] = JsonCodecMaker.make[Expendable]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Measurable] = JsonCodecMaker.make[Measurable]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[Account] = JsonCodecMaker.make[Account]
+  given JsonValueCodec[Profile] = JsonCodecMaker.make[Profile]
+  given JsonValueCodec[Edible] = JsonCodecMaker.make[Edible]
+  given JsonValueCodec[Drinkable] = JsonCodecMaker.make[Drinkable]
+  given JsonValueCodec[Expendable] = JsonCodecMaker.make[Expendable]
+  given JsonValueCodec[Measurable] = JsonCodecMaker.make[Measurable]
 
   given JsonValueCodec[Command] = JsonCodecMaker.make[Command]( CodecMakerConfig.withDiscriminatorFieldName(None) )
   given JsonValueCodec[License] = JsonCodecMaker.make[License]( CodecMakerConfig.withDiscriminatorFieldName(None) )
