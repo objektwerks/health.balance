7455389045ad6c7e089ad53c49d541631a897944
objektwerks <objektwerks@runbox.com>
2024-09-04 09:18:42 -0400

    edited dispatcher

--- server/src/main/scala/objektwerks/Dispatcher.scala
+++ server/src/main/scala/objektwerks/Dispatcher.scala
@@ -173,14 +173,13 @@ final class Dispatcher(store: Store, emailer: Emailer):
       case NonFatal(error) => Fault("Add drinkable failed:", error)
 
   private def updateDrinkable(drinkable: Drinkable)(using IO): Event =
-    Try:
+    try
       DrinkableUpdated(
         supervised:
           retry( RetryConfig.delay(1, 100.millis) )( store.updateDrinkable(drinkable) )
       )
-    .recover:
+    catch
       case NonFatal(error) => Fault("Update drinkable failed:", error)
-    .get
 
   private def listExpendables(profileId: Long)(using IO): Event =
     Try:
