32bbd2a227ddaf377deffea66ce3db5cb910716b
objektwerks <objektwerks@runbox.com>
2024-09-04 09:21:46 -0400

    edited dispatcher

--- server/src/main/scala/objektwerks/Dispatcher.scala
+++ server/src/main/scala/objektwerks/Dispatcher.scala
@@ -236,10 +236,9 @@ final class Dispatcher(store: Store, emailer: Emailer):
       case NonFatal(error) => Fault("Updated measurable failed:", error)
 
   private def addFault(fault: Fault)(using IO): Event =
-    Try:
+    try
       supervised:
         retry( RetryConfig.delay(1, 100.millis) )( store.addFault(fault) )
         FaultAdded()
-    .recover:
+    catch
       case NonFatal(error) => Fault("Add fault failed:", error)
-    .get
