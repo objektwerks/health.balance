570bc7b8eff402212ce29d2aad5dfe1cf41d895b
objektwerks <objektwerks@runbox.com>
2025-10-20 10:30:32 -0400

    remove debug

--- client/src/main/scala/objektwerks/Model.scala
+++ client/src/main/scala/objektwerks/Model.scala
@@ -37,10 +37,6 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
   val observableMeasurables = ObservableBuffer[Measurable]()
   val observableFaults = ObservableBuffer[Fault]()
 
-  objectAccount.onChange { (_, oldAccount, newAccount) =>
-    logger.info("*** object account onchange event: {} -> {}", oldAccount, newAccount)
-  }
-
   observableProfiles.onChange { (_, changes) =>
     logger.info("*** observable profiles onchange event: {}", changes)
   }
