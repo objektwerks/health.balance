b52e78cc598269f2033b780b070abbbce350e65c
objektwerks <objektwerks@runbox.com>
2025-10-28 13:45:34 -0400

    edited import

--- client/src/main/scala/objektwerks/dialog/ExpendableDialog.scala
+++ client/src/main/scala/objektwerks/dialog/ExpendableDialog.scala
@@ -7,9 +7,8 @@ import scalafx.scene.control.{ButtonType, CheckBox, ComboBox, DatePicker, Dialog
 import scalafx.scene.control.ButtonBar.ButtonData
 
 import objektwerks.{Client, Context, Expendable, ExpendableKind, Entity}
-import objektwerks.control.TimePicker
+import objektwerks.control.{IntTextField, TimePicker}
 import objektwerks.layout.ControlGridPane
-import objektwerks.control.IntTextField
 
 final class ExpendableDialog(context: Context, expendable: Expendable) extends Dialog[Expendable]:
   initOwner(Client.stage)
